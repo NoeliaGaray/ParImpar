cf41a0a23c4df619c7dfe15232aa0aaac956c6bd
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-05-09 18:11:10 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+rótulo.innerHTML = "Definir par o impar";
+let número: number = Number(dato1.value);
+let divisor: number = 2;
+let división: number = número % divisor;
+let cero: number = 0;
+let btnEnv = document.getElementById("btnEnviar");
+
+btnEnv.addEventListener("click", () => {
+  let btnEnv = document.getElementById("btnEnviar");
+  let número: number = Number(dato1.value);
+  let divisor: number = 2;
+  let división: number = número % divisor;
+  let cero: number = 0;
+  if (división === cero) {
+    console.log("El número es par");
+  } else {
+    console.log("El número es impar");
+  }
+  if (número === 0) {
+    console.log("El número es cero");
+  }
+});
